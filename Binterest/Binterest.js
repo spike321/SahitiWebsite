27415d3b6164b27dcc5c9b933944532317a5b1e4
Spike321 <sseemaku@gmail.com>
2015-07-15 18:59:45 -0400

    added draw polygon, get coordinates and nav bar with search for bin numbers

--- Binterest/Binterest.js
+++ Binterest/Binterest.js
@@ -1,11 +1,10 @@
-alert("I am an alert box!");
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2FoaXRpIiwiYSI6Ijk1ZjFlZWM2ZWVlYzg4NDExZTQzMzFiMGY1NWRlNTM3In0.mG1hNAPjaF361OcslYgq9Q';
 var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([38.89399, -77.03659], 17);
 
 var featureGroup = L.featureGroup().addTo(map);
-
+map.addLayer(featureGroup);
 var drawControl = new L.Control.Draw({
   edit: {
     featureGroup: featureGroup
@@ -19,8 +18,25 @@ var drawControl = new L.Control.Draw({
   }
 }).addTo(map);
 
-map.on('draw:created', showPolygonArea);
-map.on('draw:edited', showPolygonAreaEdited);
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+map.on('draw:created', function (e) {
+
+    var type = e.layerType,
+        layer = e.layer;
+
+   
+        layer.on('mouseover', function() {
+            alert(layer.getLatLngs());    
+        });
+    
+
+    drawnItems.addLayer(layer);
+});
+
+//map.on('draw:created', showPolygonArea);
+//map.on('draw:edited', showPolygonAreaEdited);
 
 function showPolygonAreaEdited(e) {
   e.layers.eachLayer(function(layer) {
@@ -33,3 +49,4 @@ function showPolygonArea(e) {
   e.layer.bindPopup((LGeo.area(e.layer) / 1000000).toFixed(2) + ' km<sup>2</sup>');
   e.layer.openPopup();
 }
+
