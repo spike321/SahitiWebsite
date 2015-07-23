80119baa49410ab23154cdc4b6b3ffc795eed2c8
Spike321 <sseemaku@gmail.com>
2015-07-22 22:02:17 -0400

    made a slider, read slider input, able to send to http send post , only need to improve UI, not important

--- Binterest/Binterest.js
+++ Binterest/Binterest.js
@@ -1,5 +1,4 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2FoaXRpIiwiYSI6Ijk1ZjFlZWM2ZWVlYzg4NDExZTQzMzFiMGY1NWRlNTM3In0.mG1hNAPjaF361OcslYgq9Q';
-var url = 'https://api.github.com/repos/mapbox/mapbox.js/contents/test/manual/example.geojson';
 
 var map = L.mapbox.map('map', 'mapbox.streets')
   .setView([30.05899, -95.3698], 10)
@@ -53,17 +52,35 @@ function chkArea(e) {
   }else{
 	var bounds=map.getBounds();
 	var nw=bounds.getNorthWest();
+	var nw_str = nw.toString();
+	var partsArray = nw_str.split(',');
+	var nwlatitude = partsArray[0].match(/-?\d+.?\d+/)[0];
+	var nwlongitude = partsArray[1].match(/-?\d+.?\d+/)[0];
+
 	var se=bounds.getSouthEast();
-	e.layer.bindPopup("NW: " + nw + " SE: " + se);
+	var nw_str = nw.toString();
+	var partsArray = nw_str.split(',');
+	var selatitude = partsArray[0].match(/-?\d+.?\d+/)[0];
+	var selongitude = partsArray[1].match(/-?\d+.?\d+/)[0];
+	
+	e.layer.bindPopup("NW: " + nwlatitude  + ' ,' + nwlongitude + '\n' + "SE: " + selatitude + ' ,' + selongitude );
   }
    e.layer.openPopup();
+   //sendJSON(nwlatitude, nwlongitude, selatitude, selongitude);
+   nbins = document.getElementById('bins').value;
+   //nbins = document.getElementsByClassName("searchField")[0].value;
+   alert("You selected " + nbins + " many bin locations.");
+   var values = [nwlatitude, nwlongitude, selatitude, selongitude, nbins];
+   //postToURL(url, values);
 } 
  
-function load() {
+function loadImportedGeoJson() {
   // Fetch just the contents of a .geojson file from GitHub by passing
   // `application/vnd.github.v3.raw` to the Accept header
   // As with any other AJAX request, this technique is subject to the Same Origin Policy:
   // http://en.wikipedia.org/wiki/Same_origin_policy the server delivering the request should support CORS.
+  var url = 'https://api.github.com/repos/mapbox/mapbox.js/contents/test/manual/example.geojson';
+
   $.ajax({
     headers: {
       'Accept': 'application/vnd.github.v3.raw'
@@ -80,4 +97,64 @@ function load() {
   });
 }
 
-$(load);
+function sendJSON(nwlatitude, nwlongitude, selatitude, selongitude) {
+	var geoJsonData = 
+	{
+	  "type": "FeatureCollection",
+	  "features": [
+		{
+		  "type": "Feature",
+		  "properties": {},
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [
+			  nwlatitude,
+			  nwlongitude
+			]
+		  }
+		},
+		{
+		  "type": "Feature",
+		  "properties": {},
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [
+			  selatitude,
+			  selongitude
+			]
+		  }
+		}
+	  ]
+	};	
+}
+
+function postToURL(url, values) {
+    values = values || {};
+
+    var form = createElement("form", {action: url,
+                                      method: "POST",
+                                      style: "display: none"});
+    for (var property in values) {
+        if (values.hasOwnProperty(property)) {
+            var value = values[property];
+            if (value instanceof Array) {
+                for (var i = 0, l = value.length; i < l; i++) {
+                    form.appendChild(createElement("input", {type: "hidden",
+                                                             name: property,
+                                                             value: value[i]}));
+                }
+            }
+            else {
+                form.appendChild(createElement("input", {type: "hidden",
+                                                         name: property,
+                                                         value: value}));
+            }
+        }
+    }
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+}
+
+$(loadImportedGeoJson);
+$(sendJSON);
